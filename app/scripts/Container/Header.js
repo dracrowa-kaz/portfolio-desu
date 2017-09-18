1c73c40a2edd1cda5bdb447105e1605e71d348cb
sato kazuki <dracrowa@gmail.com>
2017-09-18 21:27:58 +0300

    temp commit.

--- app/scripts/Container/Header.js
+++ app/scripts/Container/Header.js
@@ -8,11 +8,47 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import FlatButton from 'material-ui/FlatButton'
+import Toggle from 'material-ui/Toggle'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+import Drawer from 'material-ui/Drawer';
+
+const Logged = (props) => (
+  <IconMenu
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Refresh" />
+    <MenuItem primaryText="Help" />
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+)
+
+const homeButton = () => (
+  <IconButton tooltip="Font Icon">
+    <FontIcon className="muidocs-icon-action-home" />
+  </IconButton>
+)
 
 const Header = () => (
   <nav>
     <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)} >
-      <AppBar title="Home" />
+      <AppBar title="Home"
+        iconElementRight={<Logged />}
+        />
+    </MuiThemeProvider>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)} >
+      <Drawer open={true} >
+        <MenuItem>Menu Item</MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+      </Drawer>
     </MuiThemeProvider>
     <ul>
         <li><Link to="/home">HOME</Link></li>
