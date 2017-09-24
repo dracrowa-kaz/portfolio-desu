d72d5e21ae6b0bbd5691e064066c731b3c1a33ef
sato kazuki <dracrowa@gmail.com>
2017-09-24 11:19:29 +0300

    Refactoring.

--- app/scripts/Container/Header.js
+++ app/scripts/Container/Header.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
   Redirect
 } from 'react-router-dom'
 import { withRouter } from 'react-router'
@@ -11,13 +8,8 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import AppBar from 'material-ui/AppBar'
-import IconButton from 'material-ui/IconButton'
-import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import FlatButton from 'material-ui/FlatButton'
-import Toggle from 'material-ui/Toggle'
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
-import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import Drawer from 'material-ui/Drawer'
 
 class Header extends Component {
