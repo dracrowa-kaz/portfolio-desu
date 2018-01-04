c23ceb5662942a7ac853a85918ae8fe6cedefe7b
sato kazuki <dracrowa@gmail.com>
2018-01-04 10:03:51 +0200

    Manage favicon from React.

--- app/scripts/container/App.js
+++ app/scripts/container/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Favicon from 'react-favicon'
 import Login from './Login'
 import Register from './Register'
 import Home from './Home'
@@ -8,6 +9,7 @@ const App = () => (
   <div>
     <Router>
       <div>
+        <Favicon url="https://raw.githubusercontent.com/dracrowa-kaz/portfolio-desu/master/app/assets/images/favicon.ico" />
         <Route path="/home" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
